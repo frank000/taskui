2c39ddbe0785669fdec9807dadad7b7b3fb8204a
franklim.araujo <franklim.ti@gmail.com>
2020-09-19 21:42:18 -0300

    [init] implements create atividade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Home') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div>
+                        <x-jet-application-logo class="block h-12 w-auto" />
+                    </div>
+
+                    <div class="mt-8 text-2xl">
+                        Bem vindo ao seu gerente de agendamentos!
+                    </div>
+
+                    <div class="mt-6 text-gray-500">
+                        Nossa missão primeira é de impressionar seus clientes com a comodidade para agendar os seus serviços. Não medimos esforços para oferecer simplicidade
+                        na disponibilização de agendas para a atividades quais quer que você precisar.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
